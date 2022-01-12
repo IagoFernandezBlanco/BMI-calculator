c6eb18cb2d336847b352acff06d4360b22d323a3
Iagofernandez <43774731+Iagofernandez@users.noreply.github.com>
2022-01-12 14:03:25 +0100

    Ejercicio de la calculadora BMI solucionado

---
+++ README.md
@@ -0,0 +1,13 @@
+# Calculadora IMC
+
+Implementa esta calculadora del IMC. [Demo](https://omiras.github.io/BMI-calculator/)
+Puedes seguir la formula:
+
+```js
+const imc = weight / ((height * height) / 10000);
+```
+
+Intenta NO modificar el HTML. Usa selectores CSS para conseguirlo.
+
+Los datos a los cuales accedemos mediante JavaScript están dentro de un tag **form**.
+¿Qué es el evento [submit](https://javascript.info/forms-submit)?
---
+++ script.js
@@ -0,0 +1,63 @@
+const form = document.querySelector('form');
+
+form.addEventListener('submit', function (e) {
+
+  // Para que sirve 'preventDefault'? Intenta averiguarlo
+  /*
+    Este evento permite cancelar un evento(siempre que se pueda cancelar), 
+    pero sin interrumpir el resto del evento
+  */
+  e.preventDefault();
+
+  console.log("Valor del input: ", e.target.value)
+
+  // Tu código aquí --->
+
+  /*
+  1. Necesito el DOM de los elementos con los que voy a interactuar
+    Boton -> button
+    Input -> #weight
+             #height
+    Div ->   #results 
+    
+  2. Función con un addListener asociado al boton, que al invocarlo
+     recoja el valor dentro de los input, los pase a numero, haga la operacion 
+     const imc = weight / ((height * height) / 10000);  y su resultado(imc) lo escriba en el div
+  */
+  
+  let pesoIMC = document.querySelector("#weight");
+  let alturaIMC = document.querySelector("#height");
+  let resultadoIMC = document.querySelector("#results");
+
+ 
+  let peso = pesoIMC.value;
+  let altura = alturaIMC.value;
+
+  const imc =(peso /((altura* altura) /10000)).toFixed(2);
+
+  console.log(altura);
+  console.log(peso);
+  resultadoIMC.textContent = imc;
+
+  if (imc<18.6){
+    document.querySelector("form p:nth-child(2)").style.color = "red";
+    document.querySelector("form #weight-guide p:nth-child(2)").style.color ="red"
+    document.querySelector("form #weight-guide p:nth-child(3)").style.color ="black"
+    document.querySelector("form #weight-guide p:nth-child(4)").style.color ="black"
+
+
+  }else if(imc>=18.6 && imc<24.9){
+    document.querySelector("form p:nth-child(2)").style.color = "green";
+    document.querySelector("form #weight-guide p:nth-child(2)").style.color ="black"
+    document.querySelector("form #weight-guide p:nth-child(3)").style.color ="green"
+    document.querySelector("form #weight-guide p:nth-child(4)").style.color ="black"
+
+  }else{
+    document.querySelector("form p:nth-child(2)").style.color = "red";
+    document.querySelector("form #weight-guide p:nth-child(2)").style.color ="black"
+    document.querySelector("form #weight-guide p:nth-child(3)").style.color ="black"
+    document.querySelector("form #weight-guide p:nth-child(4)").style.color ="red"
+
+  }
+
+});
